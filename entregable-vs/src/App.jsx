245494a89c14a336d9fe7ff3e8497f6f8853e10c
vanisimon <vaninasimon@outlook.com>
2023-08-16 22:27:52 -0300

    sinterminar

---
+++ entregable-vs/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card'
+import Form from './Form'
+
+
+function App() {
+  const [jugador, setJugador] = useState([])
+
+  const addPlayer= (player) => {
+    setJugador([...jugador,player]);
+    console.log(jugador)
+  };
+
+  return (
+    <>
+     <div className="App">
+      <h1>Anotarse al Partido</h1>
+      <p>Rival: Camada 4</p>
+      <p>Miercoles 21 hs</p>
+      <Form onAddPlayer={addPlayer}/>
+      <Card Anotado={jugador}/>
+      
+      </div>
+      
+
+     
+    </>
+  )
+}
+
+export default App
